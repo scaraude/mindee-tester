2772d6f913711b863e9d344c54f1b160c9103bf7
scaraude <ludodante@hotmail.fr>
2022-05-05 14:22:51 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import { AnnotationViewer, getImagesFromPDF } from 'react-mindee-js'
+
+import dummyPDF from './note-de-frais.pdf'
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [selectedImageIndex, setSelectedImageIndex] = useState(0);
+
+  useEffect(() => {
+    console.log(dummyPDF, typeof dummyPDF);
+    getImagesFromPDF("http://localhost:9000/static/organisations/6d883f43-7a9a-459d-925f-f55be9efae25/documents-to-control/77c01714-10d5-451c-9e1e-1d14378471f1.pdf").then((images) => {
+      console.log("images", images, typeof images);
+      setImages(images);
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div style={{ display: "flex" }}>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        {images.map((image, key) => (
+          <img
+            src={image}
+            alt="thumbnail"
+            key={key}
+            onClick={() => setSelectedImageIndex(key)}
+            width="100vw"
+          />
+        ))}
+      </div>
+      <AnnotationViewer
+        style={{ width: "100%", height: 600, background: "black" }}
+        data={{ image: images[selectedImageIndex] }}
+      />
+    </div>)
 }
 
 export default App;
